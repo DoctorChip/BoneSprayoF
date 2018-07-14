576e3b2d3c608a4d66379ce36189f86cfac903c4
Chris Walker <chris@chriswalker.soy>
2018-07-14 18:34:32 +0100

    working midi-data in

--- BoneSpray/src/jack/jack_interface.cpp
+++ BoneSpray/src/jack/jack_interface.cpp
@@ -23,8 +23,8 @@ namespace jack
 	} midimsg;
 
 	jack_port_t *in_port;
-	jack_port_t *midi_ports;
-	jack_port_t *midi_ports_out;
+	int midi_port_count;
+	jack_port_t **midi_ports;
 	jack_client_t *client;
 	jack_ringbuffer_t *rb = NULL;
 	uint64_t monotonic_cnt = 0;
@@ -34,39 +34,47 @@ namespace jack
 	 */
 	int process(jack_nframes_t nframes, void *arg)
 	{
-		jack_default_audio_sample_t *buffer;
+		jack_default_audio_sample_t *audio_buffer;
+		jack_default_audio_sample_t *midi_buffer;
 		jack_nframes_t N;
 		jack_nframes_t i;
+		int c;
 
 		// Audio
-		buffer = (jack_default_audio_sample_t *) jack_port_get_buffer(in_port, nframes);
+		audio_buffer = (jack_default_audio_sample_t *) jack_port_get_buffer(in_port, nframes);
 
 		// Midi
-		N = jack_midi_get_event_count(buffer);
-		for (i = 0; i < N; ++i) {
+		for (c = 0; c < midi_port_count; c++)
+		{
+			jack_port_t* port = midi_ports[c];
+			midi_buffer = (jack_default_audio_sample_t *) jack_port_get_buffer(port, nframes);
 
-			jack_midi_event_t event;
-			int r;
-			r = jack_midi_event_get(&event, buffer, i);
+			N = jack_midi_get_event_count(midi_buffer);
+			for (i = 0; i < N; ++i) {
 
-			if (r == 0 && jack_ringbuffer_write_space(rb) >= sizeof(midimsg)) {
-				midimsg m;
-				m.tme_mon = monotonic_cnt;
-				m.tme_rel = event.time;
-				m.size = event.size;
-				memcpy(m.buffer, event.buffer, MAX(sizeof(m.buffer), event.size));
-				jack_ringbuffer_write(rb, (const char*)&m, sizeof(midimsg));
+				jack_midi_event_t event;
+				int r;
+				r = jack_midi_event_get(&event, midi_buffer, i);
 
-				size_t j;
-				for (j = 0; j < m.size && j < sizeof(m.buffer); ++j) {
-					printf(" %02x", m.buffer[j]);
+				if (r == 0 && jack_ringbuffer_write_space(rb) >= sizeof(midimsg)) {
+					midimsg m;
+					m.tme_mon = monotonic_cnt;
+					m.tme_rel = event.time;
+					m.size = event.size;
+					memcpy(m.buffer, event.buffer, MAX(sizeof(m.buffer), event.size));
+					jack_ringbuffer_write(rb, (const char*)&m, sizeof(midimsg));
+
+					size_t j;
+					for (j = 0; j < m.size && j < sizeof(m.buffer); ++j) {
+						printf(" %02x", m.buffer[j]);
+					}
 				}
 			}
 		}
 
 		monotonic_cnt += nframes;
 
-		//std::cout << *buffer << std::endl;
+		//std::cout << *audio_buffer << std::endl;
 		return 0;
 	}
 
@@ -109,23 +117,19 @@ namespace jack
 			return;
 		}
 
-		jack_port_t *ports[MAX_MIDI_COUNT];
-		for (int i = 0; i < MAX_MIDI_COUNT; i++) ports[i] = NULL;
-		midi_ports = *ports;
+		midi_port_count = count;
 
-		jack_port_t *ports_out[MAX_MIDI_COUNT];
-		for (int i = 0; i < MAX_MIDI_COUNT; i++) ports_out[i] = NULL;
-		midi_ports_out = *ports_out;
+		jack_port_t **ports = new jack_port_t*[MAX_MIDI_COUNT];
+		for (int i = 0; i < MAX_MIDI_COUNT; i++) ports[i] = NULL;
 
 		for (int i = 0; i < count; i++)
 		{
 			std::string name = "MIDI_" + std::to_string(i);
-			std::string name_out = "MIDI_OUT_" + std::to_string(i);
 			jack_port_t *midi = jack_port_register(client, name.c_str(), JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
-			jack_port_t *midi_out = jack_port_register(client, name_out.c_str(), JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0);
 			ports[i] = midi;
-			ports_out[i] = midi_out;
 		}
+
+		midi_ports = ports;
 	}
 
 	/*
